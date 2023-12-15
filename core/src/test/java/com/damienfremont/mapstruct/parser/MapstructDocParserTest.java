8233d7b14ee360c9133490391015e1998bb2659c
Damien FREMONT <damien.fremont@gmail.com>
2023-12-15 22:29:59 +0100

    test: 💍 core - model parser

---
+++ core/src/test/java/com/damienfremont/mapstruct/parser/MapstructDocParserTest.java
@@ -0,0 +1,32 @@
+package com.damienfremont.mapstruct.parser;
+
+import com.damienfremont.mapstruct.model.MapperModel;
+import com.damienfremont.mapstruct.model.MappingModel;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class MapstructDocParserTest {
+
+  MapstructDocParser classUnderTest = new MapstructDocParser();
+
+  @Test
+  void parse() throws Exception {
+    // GIVEN
+    ClassLoader cl = this.getClass().getClassLoader();
+    Class<?> c0 = cl.loadClass("com.damienfremont.mapstruct.example.getstarted.CarMapper");
+    // WHEN
+    List<MapperModel> res = classUnderTest.parse(c0);
+    // THEN
+    MapperModel res0 = res.get(0);
+    assertEquals("CarMapper", res0.getName());
+    assertEquals_MappingModel("carToCarDto", null, res0.getMappings().get(0));
+  }
+
+  private void assertEquals_MappingModel(String carToCarDto, Short expected, MappingModel res0map0) {
+    assertEquals(carToCarDto, res0map0.getSource());
+    assertEquals(expected, res0map0.getTarget());
+  }
+}
