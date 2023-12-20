3d691a3466f04f168c961b0ff1e94da86897178d
Damien FREMONT <damien.fremont@gmail.com>
2023-12-20 22:25:40 +0100

    feat: 🎸 annotation reader - core

--- core/src/main/java/com/damienfremont/mapstruct/parser/MapstructDocParser.java
+++ core/src/main/java/com/damienfremont/mapstruct/parser/MapstructDocParser.java
@@ -7,10 +7,21 @@ package com.damienfremont.mapstruct.parser;
 
 import com.damienfremont.mapstruct.model.MapperModel;
 import com.damienfremont.mapstruct.model.MappingModel;
+import org.mapstruct.Mapping;
+import org.objectweb.asm.ClassReader;
+import org.objectweb.asm.tree.AnnotationNode;
+import org.objectweb.asm.tree.ClassNode;
+import org.objectweb.asm.tree.MethodNode;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.reflect.Field;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.List;
+import java.util.stream.Stream;
 
 import static java.util.Arrays.stream;
 import static java.util.stream.Collectors.toList;
@@ -19,28 +30,73 @@ public class MapstructDocParser {
 
   public List<MapperModel> parse(Class<?> include) {
     try {
-      MapperModel m0 = new MapperModel();
-      m0.setName(include.getSimpleName());
-      m0.setClazz(include);
-      m0.setMappings(parseMethods(include.getMethods()));
-      return Arrays.asList(m0);
+      Method m0 = include.getMethods()[0];
+      MapperModel mm0 = new MapperModel();
+      mm0.setName(include.getSimpleName());
+      mm0.setClazz(include);
+      mm0.setMappings(parseMappings(m0));
+      return Arrays.asList(mm0);
     } catch (Exception e) {
       throw new IllegalArgumentException(e);
     }
   }
 
-  List<MappingModel> parseMethods(Method[] methods) {
-    return stream(methods)
-            .filter(this::hasMappingAnnotation)
-            .map(this::toMappingModel)
+  private List<MappingModel> parseMappings(Method m0) throws IOException {
+    List<MappingModel> moms = Stream
+            .concat(
+                    parseDefaultMappings(m0).stream(),
+                    parseAnnotatMappings(m0).stream())
+            .sorted(Comparator.comparing(s -> s.getTarget().toLowerCase()))
+            .collect(toList());
+    return moms;
+  }
+
+  List<MappingModel> parseDefaultMappings(Method method) {
+    Class<?> defTgt = method.getReturnType();
+    Class<?> defSrc = method.getParameterTypes()[0];
+    Field[] defTgtFs = defTgt.getDeclaredFields();
+    Field[] defSrcFs = defSrc.getDeclaredFields();
+    return stream(defTgtFs)
+            .filter(i -> sourceFieldExists(i, defSrcFs))
+            .map(i -> toMappingModel(i))
             .collect(toList());
   }
 
-  MappingModel toMappingModel(Method m) {
-    return new MappingModel(m.getName(), null);
+  // ASM Tree API https://www.javacodegeeks.com/2012/02/manipulating-java-class-files-with-asm_22.html
+  List<MappingModel> parseAnnotatMappings(Method m0) throws IOException {
+    List<MappingModel> res = new ArrayList<>();
+    Class<Mapping> mappingClass = Mapping.class;
+    String name = "/" + m0.getDeclaringClass().getName().replaceAll("\\.", "/") + ".class";
+    InputStream in = MapstructDocParser.class.getResourceAsStream(name);
+    ClassReader cr = new ClassReader(in);
+    ClassNode classNode = new ClassNode();
+    cr.accept(classNode, 0);
+    for (MethodNode methodNode : classNode.methods) {
+      if (methodNode.invisibleAnnotations != null) {
+        for (AnnotationNode annotationNode : methodNode.invisibleAnnotations) {
+          if (annotationNode.desc.equals("L" + mappingClass.getName().replaceAll("\\.", "/") + ";")) {
+            String srcVal = annotationNode.values.get(1).toString();
+            String tgtVal = annotationNode.values.get(3).toString();
+            res.add(new MappingModel(srcVal, tgtVal));
+          }
+        }
+      }
+    }
+    return res;
   }
 
-  boolean hasMappingAnnotation(Method m) {
-    return true;
+  static boolean sourceFieldExists(Field field, Field[] defSrcFs) {
+    return stream(defSrcFs)
+            .filter(f -> equals(f.getName(), field.getName()))
+            .findAny()
+            .isPresent();
+  }
+
+  static MappingModel toMappingModel(Field f) {
+    return new MappingModel(f.getName(), f.getName());
+  }
+
+  static boolean equals(String s1, String s2) {
+    return s1.equals(s2);
   }
 }
--- core/src/test/java/com/damienfremont/mapstruct/parser/MapstructDocParserTest.java
+++ core/src/test/java/com/damienfremont/mapstruct/parser/MapstructDocParserTest.java
@@ -20,13 +20,22 @@ class MapstructDocParserTest {
     // WHEN
     List<MapperModel> res = classUnderTest.parse(c0);
     // THEN
-    MapperModel res0 = res.get(0);
-    assertEquals("CarMapper", res0.getName());
-    assertEquals_MappingModel("carToCarDto", null, res0.getMappings().get(0));
+    assertEquals("CarMapper", getMappingName(res));
+    assertEquals_MappingModel("make", "make", getMappingModel(res, 0));
+    assertEquals_MappingModel("numberOfSeats", "seatCount", getMappingModel(res, 1));
+    assertEquals_MappingModel("type", "type", getMappingModel(res, 2));
   }
 
   private void assertEquals_MappingModel(String carToCarDto, String expected, MappingModel res0map0) {
     assertEquals(carToCarDto, res0map0.getSource());
     assertEquals(expected, res0map0.getTarget());
+  }
+
+  private String getMappingName(List<MapperModel> res) {
+    return res.get(0).getName();
+  }
+
+  private MappingModel getMappingModel(List<MapperModel> res, int index) {
+    return res.get(0).getMappings().get(index);
   }
 }
